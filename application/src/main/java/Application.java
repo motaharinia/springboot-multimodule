4a7f34de2d5d9f54c3d46894c40dd05473fb316f
MostafaMotaharinia <eng.motahari@gmail.com>
2020-10-22 01:06:52 +0330

    initial commit

---
+++ application/src/main/java/Application.java
@@ -0,0 +1,6 @@
+import org.springframework.boot.SpringApplication;
+
+
+public class Application {
+
+}
---
+++ src/test/java/ParentTest.java
@@ -0,0 +1,8 @@
+/**
+ * User: https://github.com/motaharinia<br>
+ * Date: 2020-10-22 <br>
+ * Time: 01:05:55 <br>
+ * Description: <br>
+ */
+public class ParentTest {
+}
